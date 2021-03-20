e57bd6eb61cc56cde9ed9f22d8099f68ef153d7f
Chris Miller <miller04@gmail.com>
2021-03-20 13:52:16 +0000

    Created a basic custom logger

---
+++ Logger.js
@@ -0,0 +1,21 @@
+﻿const DEBUG_ENABLED = true
+
+const logger = {
+    log: (...args) => {
+        if(DEBUG_ENABLED) console.log(...args)
+    },
+    warn: (...args) => {
+        if(DEBUG_ENABLED) console.warn(...args)
+    },
+    error: (...args) => {
+        if(DEBUG_ENABLED) console.error(...args)
+    },
+    info: (...args) => {
+        if(DEBUG_ENABLED) console.info(...args)
+    },
+    trace: (...args) => {
+        if(DEBUG_ENABLED) console.trace(...args)
+    }
+}
+
+export default logger
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Javascript-Toolbox
 Javascript Toolbox
+
+1. Logger.js - A custom logger that allows logs to be turned off globally.
